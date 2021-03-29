703c62d0effa4a12a3ba41f281b00a91f0617545
Romain <romain.chagnaud@etu.u-bordeaux.fr>
2021-03-29 18:33:33 +0200

    Test

--- TestGit/src/testgit/TestGit.java
+++ TestGit/src/testgit/TestGit.java
@@ -15,7 +15,7 @@ public class TestGit {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
+        System.out.println("hello word");
     }
      
 }
