593c69fd23ef2fb1cfb4bfdf24ab6446c8608792
Romain <romain.chagnaud@etu.u-bordeaux.fr>
2021-03-30 08:18:30 +0200

    Board

--- ProjetPOOGomoku/src/Board/Board.java
+++ ProjetPOOGomoku/src/Board/Board.java
@@ -19,38 +19,47 @@ public class Board extends Game {
         this.Cols = col;
         this.Rows = lig;
     }
-    
-    
+
     public void display() {
-        for(int x = 0; x <Rows; x++){
-            alphabet();
-        }
+        alphabet();
+        System.out.println(" ");
         Bar();
         for (int r = 0; r < Rows; r++) {
             System.out.print("|");
             for (int c = 0; c < Cols; c++) {
-                System.out.print(" "); 
+                System.out.print(" ");
             }
             System.out.println("|");
         }
         Bar();
+        System.out.println(" ");
+       chiffre();
     }
 
     /**
      * affiche l'image
      */
     private void Bar() {
+
         System.out.print("+");
         for (int c = 0; c < Cols; c++) {
             System.out.print("-");
         }
         System.out.println("+");
     }
-    
-    private void alphabet(){
-        for(char c ='a'; c <= 'z'; c++){
-            System.out.println(c);
+
+    private void alphabet() {
+        ArrayList tab = new ArrayList();
+        for (char c = 'A'; c <= 'Z'; c++) {
+            tab.add(c);
+            System.out.print(c);
         }
     }
     
+    private void chiffre() {
+        for(int d = 0; d <= 100; d++){
+            System.out.print(d);
+        }
+    }
 }
+
