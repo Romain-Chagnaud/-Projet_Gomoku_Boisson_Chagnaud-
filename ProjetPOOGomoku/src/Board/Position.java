4719975963d904e9e685e737a25e21ae9d78143c
Manon <manon.boisson.1@iut.u-bordeaux.fr>
2021-03-30 09:07:02 +0200

    modification  classe position

---
+++ ProjetPOOGomoku/src/Board/Position.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Board;
+
+/**
+ *
+ * @author manon
+ */
+public class Position {
+    
+    public char row;
+    public int col;
+    
+    public Position (char row, int col) {
+        this.row = row;
+        this.col = col;
+    }
+    
+   
+}
