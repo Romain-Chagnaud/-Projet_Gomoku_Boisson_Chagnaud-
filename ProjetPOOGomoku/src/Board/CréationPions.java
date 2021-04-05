2cc8a2a6cbfa6c57317bb18806ce5d4a133dca4f
Romain <romain.chagnaud@etu.u-bordeaux.fr>
2021-04-05 12:04:51 +0200

    Modif CréationPion

--- ProjetPOOGomoku/src/Board/CréationPions.java
+++ ProjetPOOGomoku/src/Board/CréationPions.java
@@ -14,8 +14,8 @@ public class CréationPions {
     public void printBoard(Board b) {
         for (int i = 0; i < b.size; i++) {
             for (int y = 0; y < b.size; y++) {
-                if (b.board[i][y] == Color.BLACK.ordinal()) {
-                    System.out.println("o"); // comparaison entre un int et une emnum
+                if (b.board[i][y] == Color.BLACK.ordinal()) { // comparaison entre un int et une emnum
+                    System.out.println("o"); 
                 } else if (b.board[i][y] == Color.WHITE.ordinal()) {
                     System.out.println("x");
                 }
